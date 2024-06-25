45b98a69485101500f13a39144bb1948fa50e6fc
StrekozovVA <strekozov_VA@hotmail.com>
2024-06-25 16:25:30 +0300

    edit mon.py

--- 10-monitoring-01-base/mon.py
+++ 10-monitoring-01-base/mon.py
@@ -2,7 +2,11 @@ import time
 import json
 filename = time.strftime("%Y-%m-%d")+"-awesome-monitoring.log"
 log = open("/var/log/"+filename,"a")
-metric1 = open('/proc/loadavg')[:14]
-out = json.dumps({'timestamp': time.strftime("%Y-%m-%d %H:%M:%S"),'loadavg': metric1.read()[:14]})
+metric1 = (open('/proc/loadavg')).read()[:14]
+metric2 = ((open('/proc/meminfo')).read()).split()[1]
+metric3 = ((open('/proc/meminfo')).read()).split()[4]
+metric4 = ((open('/proc/uptime')).read()).split()[0]
+timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+out = json.dumps({'timestamp': timestamp,'loadavg,5 10 15': metric1,'MemTotal,Gb': int(metric2)/1024,'MemFree,Gb': int(metric3)/1024,'Uptime,H': float(metric4)/60/60})
 log.write(f'\n{out}')
 log.close()
